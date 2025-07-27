770b40801d36cd80278aabd70647574215e0e912
Smb2003 <99506103+Smb2003@users.noreply.github.com>
2025-07-27 20:47:08 +0500

    Fix db url issue

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const {DB_NAME} = require("../constant.js");
 
 const connect_to_database = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://${MONGODB_NAME}:${MONGODB_PASSWORD}@cluster0.qo0gy0m.mongodb.net/${DB_NAME}`);
+        await mongoose.connect(`mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.qo0gy0m.mongodb.net/${DB_NAME}`);
 
         mongoose.connection.on("connected",()=>{
             console.log("Connected to database");
